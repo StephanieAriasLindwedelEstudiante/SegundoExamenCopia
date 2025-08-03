1fb919804104fb84c5ffbb951580041569144f74
StephanieAriasLindwedelEstudiante <step2@STEPHANIE_ARIAS>
2025-08-02 23:04:57 -0600

    Elimina la verificación de usuarios en el inicio de sesión
    Se han eliminado líneas clave en la función `verificarUsuario`, incluyendo la creación de `DatabaseHelper`, la consulta SQL y la lógica de manejo de usuarios encontrados o no. Esto simplifica la funcionalidad de inicio de sesión.

--- login.aspx.vb
+++ login.aspx.vb
@@ -1,24 +1,20 @@
 ﻿Imports System.Data.SqlClient
 Public Class login
     Inherits System.Web.UI.Page
-
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
     Protected Function verificarUsuario(usuario As Usuarios) As Usuarios
         Try
-
             Dim helper As New DatabaseHelper()
             Dim parametros As New List(Of SqlParameter) From {
                 New SqlParameter("@Email", usuario.Correo),
                 New SqlParameter("@Password", usuario.Contrasena)
             }
-
             Dim query As String = "SELECT * FROM Usuarios WHERE EMAIL = @Email AND CONTRASENIA = @Password;"
             Dim dataTable As DataTable = helper.ExecuteQuery(query, parametros)
             If dataTable.Rows.Count > 0 Then
                 Dim usuarioCompleto As Usuarios = usuario.dtToUsuarios(dataTable)
-                ' Usuario encontrado, puedes redirigir o realizar otra acción
                 Session("UsuarioId") = usuarioCompleto.Id.ToString()
                 Session("UsuarioNombre") = usuarioCompleto.Nombre.ToString()
                 Session("UsuarioApellido") = usuarioCompleto.Apellido.ToString()
@@ -26,7 +22,6 @@ Public Class login
                 Session("UsuarioRol") = usuarioCompleto.Rol_ID.ToString()
                 Return usuarioCompleto
             Else
-                ' Usuario no encontrado, manejar el error
                 Return Nothing
             End If
         Catch ex As Exception
